c171b3de368561b110e55c89084ea9ef1b71adc7
HaxagonusD <jquezad1@binghamton.edu>
2020-09-28 21:37:16 -0400

    Fisnished

--- conditionals.js
+++ conditionals.js
@@ -1,45 +1,59 @@
 //Exercise #1
 //Write a function named greaterNum that:
 // takes 2 arguments, both numbers.
-// returns whichever number is the greater (higher) number. 
+// returns whichever number is the greater (higher) number.
 // Call that function 2 times with different number pairs
-// and log the output to make sure it works 
+// and log the output to make sure it works
 //(e.g. "The greater number of 5 and 10 is 10")
 
-
-
-
-
+function greaterNum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 //Exercise #2
-//Write a function named helloWorld that: 
+//Write a function named helloWorld that:
 // takes 1 argument, a language code ("es", "de", "en")
-// returns "Hello, World" for the given language, for at least 
+// returns "Hello, World" for the given language, for at least
 // 3 languages. It should default to returning English
-//Call that function for each of the supported languages and log the 
+//Call that function for each of the supported languages and log the
 // resultto make sure it works
 
-
-
-
-
-
+function helloWorld(lang) {
+  if (lang == "fr") {
+    return "Bonjour tout le monde";
+  } else if (lang == "es") {
+    return "Hola, Mundo";
+  } else {
+    return "Hello, World";
+  }
+}
+console.log(helloWorld("en"));
+console.log(helloWorld("fr"));
+console.log(helloWorld("es"));
 
 //Exercise #3
 //Write a function named assignGrade that:
 //takes 1 argument, a number score
 //returns a grade for the score, either "A", "B", "C", "D", or "F"
-//Call that function for a few different scores and log the result 
+//Call that function for a few different scores and log the result
 //to make sure it works
-
-
-
-
-
-
-
-
-
+function assignGrade(score) {
+  if (score > 90) {
+    return "A";
+  } else if (score > 80) {
+    return "B";
+  } else if (score > 70) {
+    return "C";
+  } else if (score > 65) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
 //Exercise #4
 //Write a function named pluralize that:
 //takes 2 arguments, a noun and a number
@@ -47,3 +61,10 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+function pluralize(noun, number) {
+  if (number != 1 && noun != "sheep" && noun != "geese") {
+    return number + " " + noun + "s";
+  } else {
+    return number + " " + noun;
+  }
+}
